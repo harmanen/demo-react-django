8133f39d36db3cd927203d748cf62cef98278e7e
Jussi Harmanen <hurmanen@gmail.com>
2020-12-01 13:25:03 +0200

    Remove webpack-dev-server
    Not  working. Build a bundle for Django instead.

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,16 +1,4 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 module.exports = {
-  entry: './main.js',
-  output: {
-    path: path.join(__dirname, '/bundle'),
-    filename: 'index_bundle.js',
-  },
-  devServer: {
-    inline: true,
-    port: 8000,
-  },
   module: {
     rules: [
       {
@@ -23,9 +11,4 @@ module.exports = {
       },
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-    }),
-  ],
 };
