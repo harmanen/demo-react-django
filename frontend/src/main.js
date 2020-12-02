4c5d88c98a4a4d45ea8c2ddb5ff89a326a0454cb
Jussi Harmanen <hurmanen@gmail.com>
2020-12-02 11:44:55 +0200

    Fix an import

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import { MuiThemeProvider } from '@material-ui/core';
 import App from './App.js';
 import theme from './theme.js';
 
