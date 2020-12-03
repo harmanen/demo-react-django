cd73e1f3fd208058f1a2dfd482238f32a646ac6f
Jussi Harmanen <hurmanen@gmail.com>
2020-12-03 10:12:28 +0200

    Add a favicon.ico

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,10 +15,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic.base import RedirectView
+
+from django.contrib.staticfiles.storage import (
+    staticfiles_storage,
+)
+
 from . import views
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # frontend
     path("", views.index),
+    # favicon
+    path(
+        "favicon.ico",
+        RedirectView.as_view(
+            url=staticfiles_storage.url("favicon.ico")
+        ),
+    ),
 ]
