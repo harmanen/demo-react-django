a3ba57086daf1ef4862b347a92559915324804fd
Jussi Harmanen <hurmanen@gmail.com>
2020-12-03 17:35:47 +0200

    Wrap app with redux provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { MuiThemeProvider } from '@material-ui/core';
 import './i18n';
 import App from './App';
 import theme from './theme';
+import store from './store';
 
 ReactDOM.render(
-  <Router>
-    <MuiThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </MuiThemeProvider>
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </MuiThemeProvider>
+    </Router>
+  </Provider>,
   document.getElementById('app'),
 );
