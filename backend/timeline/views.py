8cc74c266ba87b587e7d4ec90ba8946fbbebaeb9
Jussi Harmanen <hurmanen@gmail.com>
2020-12-14 11:17:09 +0200

    Create a view for timeline

--- backend/timeline/views.py
+++ backend/timeline/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Timeline
+from .serializers import TimelineSerializer
+
+
+class TimelineViewSet(viewsets.ModelViewSet):
+    queryset = Timeline.objects.all()
+    serializer_class = TimelineSerializer
+
+    def retrieve(self, request, pk=None):
+        obj, created = Timeline.objects.get_or_create(pk=pk)
+        data = TimelineSerializer(obj).data
+
+        if created:
+            return Response(
+                data,
+                status=status.HTTP_201_CREATED,
+            )
+
+        return Response(data, status=status.HTTP_200_OK)
