f11b482fc7b33de407c675423856be295ae6dd7a
Jussi Harmanen <hurmanen@gmail.com>
2020-12-14 11:16:29 +0200

    Create a model for timeline

--- backend/timeline/models.py
+++ backend/timeline/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+
+class Timeline(models.Model):
+    RECEIVED = "RE"
+    TURNED_DOWN = "TU"
+    IN_EVALUATION = "EV"
+    REJECTED = "RE"
+    IN_DEVELOPMENT = "DE"
+    DISCONTINUED = "DI"
+    COMPLETED = "CO"
+
+    STATUS_CHOICES = (
+        (RECEIVED, "Received"),
+        (TURNED_DOWN, "Turned down"),
+        (IN_EVALUATION, "In evaluation"),
+        (REJECTED, "Rejected"),
+        (IN_DEVELOPMENT, "In development"),
+        (DISCONTINUED, "Discontinued"),
+        (COMPLETED, "Completed"),
+    )
+
+    def get_default_state():
+        return list(["RE"])
+
+    state = ArrayField(
+        models.CharField(
+            max_length=2,
+            blank=True,
+            choices=STATUS_CHOICES,
+        ),
+        default=get_default_state,
+        blank=False,
+    )
