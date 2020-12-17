f8a518ad1066ad44a4b2bc95857a0a50a84a6b92
Jussi Harmanen <hurmanen@gmail.com>
2020-12-17 15:45:03 +0200

    Add tests for Description

---
+++ frontend/src/layout/components/Description.test.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Box, Typography } from '@material-ui/core';
+import ProviderStack from '../../test/components/ProviderStack';
+import Description from './Description';
+import store from '../../store';
+import { setIsMobile } from '../slices';
+
+const description = 'Some text...';
+
+describe('Description', () => {
+  describe('Desktop', () => {
+    const wrapper = mount(
+      <ProviderStack>
+        <Description description={description} />
+      </ProviderStack>,
+    );
+
+    it('Should render Typography with correct text and variant', () => {
+      expect(
+        wrapper.find({ variant: 'body1' }).find(Typography),
+      ).to.contain.text(description);
+    });
+
+    it('Should render Box with border', () => {
+      expect(
+        wrapper
+          .find(Box)
+          .find({ className: 'makeStyles-divider-2' })
+          .exists(),
+      ).to.eql(true);
+    });
+  });
+
+  describe('Mobile', () => {
+    store.dispatch(setIsMobile(true));
+
+    const wrapper = mount(
+      <ProviderStack store={store}>
+        <Description description={description} />
+      </ProviderStack>,
+    );
+
+    it('Should render Typography with correct text and variant', () => {
+      expect(
+        wrapper.find({ variant: 'body2' }).find(Typography),
+      ).to.contain.text(description);
+    });
+
+    it('Should render Box with border', () => {
+      expect(
+        wrapper
+          .find(Box)
+          .find({ className: 'makeStyles-divider-2' })
+          .exists(),
+      ).to.eql(true);
+    });
+  });
+});
