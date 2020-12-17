d4c1c46a287b38bbc9b56e37a43dea91d515316c
Jussi Harmanen <hurmanen@gmail.com>
2020-12-17 12:12:32 +0200

    Edit admin site for timeline

--- backend/timeline/admin.py
+++ backend/timeline/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Timeline
 
-admin.site.register(Timeline)
+
+@admin.register(Timeline)
+class TimelineAdmin(admin.ModelAdmin):
+    list_display = ("id", "state")
