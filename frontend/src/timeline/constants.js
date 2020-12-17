499cd5aec904ad8a886e117bda340c76bccc9db9
Jussi Harmanen <hurmanen@gmail.com>
2020-12-17 12:13:03 +0200

    Add TIMELINE_ID to constants

--- frontend/src/timeline/constants.js
+++ frontend/src/timeline/constants.js
@@ -1,4 +1,6 @@
-// eslint-disable-next-line import/prefer-default-export
+// Timeline id is hard-coded for demo purposes...
+export const TIMELINE_ID = 1;
+
 export const STATE_CODES = {
   // Add new codes to translations
   RE: 'received',
