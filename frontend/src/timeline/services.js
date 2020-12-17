a6a0925a4701ab407b836e4da2e9df04af669566
Jussi Harmanen <hurmanen@gmail.com>
2020-12-17 12:13:21 +0200

    Add a service for updating timelines

--- frontend/src/timeline/services.js
+++ frontend/src/timeline/services.js
@@ -1,10 +1,24 @@
 import axios from 'axios';
 import { BASE_URL } from '../constants';
 
-// eslint-disable-next-line import/prefer-default-export
 export const getTimeline = async (id) => {
+  axios.defaults.xsrfCookieName = 'csrftoken';
+  axios.defaults.xsrfHeaderName = 'X-CSRFToken';
+
   const response = await axios.get(
     `${BASE_URL}timelines/${id}`,
+  );
+
+  return response.data;
+};
+
+export const updateTimeline = async (data) => {
+  axios.defaults.xsrfCookieName = 'csrftoken';
+  axios.defaults.xsrfHeaderName = 'X-CSRFToken';
+
+  const response = await axios.patch(
+    `${BASE_URL}timelines/${data.id}`,
+    { state: [data.state] },
   );
 
   return response.data;
